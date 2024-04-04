2d5b5ddf7639f12c91f690743b8cd7de9ed0293b
https-richardy <=>
2024-04-04 11:37:35 -0300

    adjustment(models): Update image upload directory for products

--- scr/store/models/product.py
+++ scr/store/models/product.py
@@ -4,7 +4,7 @@ from store.models import Category
 
 class Product(models.Model):
     title = models.CharField(max_length=120)
-    cover = models.ImageField(upload_to='imgs/products_covers/')
+    cover = models.ImageField(upload_to='imgs/products/')
     description = models.TextField()
     category = models.ForeignKey(Category, on_delete=models.CASCADE)
     price = models.DecimalField(max_digits=10, decimal_places=2)
