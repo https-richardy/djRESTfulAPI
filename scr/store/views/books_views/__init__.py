1b4e204429b236c1c1c97fd1d000e2f565a58745
https-richardy <=>
2024-04-04 12:12:48 -0300

    refactor(views): remove unnecessary packages books_views and clothings_views

--- scr/store/views/books_views/__init__.py
+++
@@ -1,9 +0,0 @@
-# flake8: noqa
-
-from .book_create_view import BookCreate
-from .book_detail_view import BookDetail
-
-from .book_list_view import BookList
-from .book_list_with_filters import BookListWithFilters
-
-from .book_update_view import BookUpdate
--- scr/store/views/clothings_views/__init__.py
+++
@@ -1,8 +0,0 @@
-# flake8: noqa
-from .clothing_create_view import ClothingCreate
-from .clothing_detail_view import ClothingDetail
-
-from .clothing_list_view import ClothingList
-from .clothing_list_with_filters_view import ClothingListWithFilters
-
-from .clothing_update_view import ClothingUpdate
