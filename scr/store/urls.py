6b7cf33c8e37910b2d5becb9e38c30e11e9306c5
https-richardy <=>
2024-04-04 12:33:32 -0300

    refactor(urls): update URL patterns and imports for product views

--- scr/store/urls.py
+++ scr/store/urls.py
@@ -1,18 +1,18 @@
 from django.urls import path
-from .views import (
-    ProductCreate,
-    ProductList,
-    ProductDetail,
-    ProductUpdate,
-    ProductDelete,
-    ProductListWithFilters,
+from .views.product_views import (
+    ListProductView,
+    ListProductsWithFiltersView,
+    CreateProductView,
+    ProductDetailsView,
+    UpdateProductView,
+    DeleteProductView,
 )
 
 urlpatterns = [
-    path('products/', ProductList.as_view(), name='products-list'),
-    path('products/search/', ProductListWithFilters.as_view(), name='product-search'),  # noqa: E501
-    path('product/<int:pk>/', ProductDetail.as_view(), name='product-detail'),
-    path('product/create/', ProductCreate.as_view(), name='product-create'),
-    path('product/<int:pk>/update/', ProductUpdate.as_view(), name='product-update'),  # noqa: E501
-    path('product/<int:pk>/delete/', ProductDelete.as_view(), name='product-delete')  # noqa: E501
+    path('products/', ListProductView.as_view(), name='products-list'),
+    path('products/search/', ListProductsWithFiltersView.as_view(), name='product-search'),  # noqa: E501
+    path('products/<int:pk>/', ProductDetailsView.as_view(), name='product-detail'),
+    path('products/', CreateProductView.as_view(), name='product-create'),
+    path('products/<int:pk>/', UpdateProductView.as_view(), name='product-update'),  # noqa: E501
+    path('products/<int:pk>/', DeleteProductView.as_view(), name='product-delete')  # noqa: E501
 ]
